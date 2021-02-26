2080487d739296c1504f055d4c5499138a00502b
pruthvi-naskanti <60023780+pruthvi-naskanti@users.noreply.github.com>
2021-02-25 22:31:26 -0600

    Update App.java

--- src/main/java/edu/nwmissouri/bigdata/section01/group4/App.java
+++ src/main/java/edu/nwmissouri/bigdata/section01/group4/App.java
@@ -15,12 +15,12 @@ public class App
 //        OddNumber odd=new OddNumber();
 //        odd.OddNumberDetection();
         
-<<<<<<< HEAD
+
         StringLength string = new StringLength();
         string.FraudDetectStringLength();
-=======
+
         Squarenumberornot odd=new Squarenumberornot();
         odd.OddNumberDetection();
->>>>>>> 6376e029b6bc1c0b513e2a83fa54b49307c59fb6
+
     }
 }
