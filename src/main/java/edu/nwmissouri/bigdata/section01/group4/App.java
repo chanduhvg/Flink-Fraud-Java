e4782043183dfc635893114e184b62b17acb2462
RaviTeja444 <pagidojuraviteja2@gmail.com>
2021-02-25 21:17:44 -0600

    Added Java Project

---
+++ src/main/java/edu/nwmissouri/bigdata/section01/group4/App.java
@@ -0,0 +1,15 @@
+package edu.nwmissouri.bigdata.section01.group4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        //Even number fraud detection
+        EvenNumber evenNumber=new EvenNumber();
+        evenNumber.FraudDetectEvenNumber();
+    }
+}
